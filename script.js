c5e4cde70169990de3edba3a75f83f36ea059e8b
Ali Jan <alijaan.web@gmail.com>
2024-06-10 10:36:11 -0700

    heade and slider

---
+++ script.js
@@ -0,0 +1,10 @@
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 0) {
+      navbar.classList.remove('bg-initial');
+      navbar.classList.add('bg-scrolled');
+    } else {
+      navbar.classList.remove('bg-scrolled');
+      navbar.classList.add('bg-initial');
+    }
+  });
